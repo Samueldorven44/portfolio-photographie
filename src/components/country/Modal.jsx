aaab042fbfdf42f07d71aa095ec8efc335d71c47
samuel dorven <sdorven@outlook.fr>
2025-10-01 12:44:32 +0200

    Pop up terminé + ajout de mapbox sur le popup

--- src/components/country/Modal.jsx
+++ src/components/country/Modal.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import '../../styles/Modal.css'
+import { useState } from 'react';
+import '../../styles/Modal.css';
 import { CircleX } from 'lucide-react';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import Map from 'react-map-gl/mapbox';
+
+const TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
 
 function Modal({ image, onClose }) {
+  const [viewState, setViewState] = useState({
+    latitude: 28.6448,
+    longitude: 77.226,
+    zoom: 5,
+  });
 
   return (
     <div className='overlay'>
       <div className='content-overlay'>
-        <button onClick={onClose} className='btn-overlay'><CircleX size={30}/></button>
+        <button onClick={onClose} className='btn-overlay'>
+          <CircleX size={30} />
+        </button>
+
         {image && (
-          <div className="modal-content">
+          <div className="content-modal">
             <div className='content-img'>
               <img
                 className='img-modal'
@@ -18,14 +31,23 @@ function Modal({ image, onClose }) {
                 loading='lazy'
               />
             </div>
-            {/* <div className='content-texte'>
+            <div className='content-text'>
               <h1>{image.name}</h1>
-            </div> */}
+              <p>{image.description}</p>
+              <div className="map-container">
+                <Map
+                  {...viewState}
+                  onMove={evt => setViewState(evt.viewState)}
+                  mapboxAccessToken={TOKEN}
+                  mapStyle="mapbox://styles/mapbox/streets-v11"
+                />
+              </div>
+            </div>
           </div>
         )}
       </div>
     </div>
-  )
+  );
 }
 
-export default Modal
+export default Modal;
